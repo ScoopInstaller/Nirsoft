fb168e0a0165cda8bec93c83a15155ea0ab434e3
MCOfficer <mcofficer@gmx.de>
2019-08-01 09:09:45 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# nirsoft-bucket
+[![Build status](https://ci.appveyor.com/api/projects/status/k8jx9v3nluqnpj4h?svg=true)](https://ci.appveyor.com/project/MCOfficer/scoop-nirsoft)
+
+A scoop bucket containing all 250+ programs from [nirsoft.net](https://www.nirsoft.net/).
+
+[Scoop](https://scoop.sh/) is a windows package manager similar to apt-get or homebrew.
+You can add this bucket to scoop using
+```
+scoop bucket add nirsoft-alternative https://github.com/MCOfficer/scoop-nirsoft.git
+```
---
+++ update.py
@@ -0,0 +1,118 @@
+import os
+import json
+import xml.etree.ElementTree as ET
+import requests
+from zipfile import ZipFile
+from io import BytesIO
+import time
+from traceback import print_exc
+
+
+def probe_for_exe(url):
+    print("Downloading " + url + "...")
+    r = requests.get(url)
+    with ZipFile(BytesIO(r.content)) as z:
+        for name in z.namelist():
+            if name.endswith(".exe"):
+                return name
+if __name__ == '__main__':
+    print("Fetching Padfile links")
+    pads = requests.get("https://www.nirsoft.net/pad/pad-links.txt").text
+
+    i = 0
+    for line in pads.splitlines():
+        i += 1
+        if i % 5 == 0:
+            print("Sleeping 5 seconds to not spam the server")
+            time.sleep(5)
+        print("")
+        print("Generating from " + line + " (" + str(i) + "/" + str(len(pads.splitlines())) + ")")
+
+        try:
+            padfile = requests.get(line).text
+            root = ET.fromstring(padfile)
+
+            info = root.find("Program_Info")
+            version = info.find("Program_Version").text
+            full_name = info.find("Program_Name").text
+
+            web_info = root.find("Web_Info")
+            website = web_info.find("Application_URLs").find("Application_Info_URL").text.replace("http:", "https:")
+            download = web_info.find("Download_URLs").find("Primary_Download_URL").text.replace("http:", "https:")
+            download64 = download.replace(".zip", "-x64.zip")
+            name = os.path.splitext(os.path.basename(line))[0]
+
+            bin = probe_for_exe(download)
+            if not bin:
+                print("No executable found! Skipping")
+
+            description = ""
+            shortcut = "NirSoft\\" + full_name
+            try:
+                descriptions = root.find("Program_Descriptions").find("English")
+                description = descriptions.find("Char_Desc_80").text
+            except AttributeError:
+                pass
+
+            print("Checking 64-bit download url")
+            r = requests.head(download64)
+            x64 = bool(r.ok)
+            if not x64:
+                print("64-bit download unavailable")
+
+
+            manifest = {
+                "version": "0",
+                "homepage": website,
+                "url": download,
+                "bin": bin,
+                "shortcuts": [
+                    [ bin, shortcut ]
+                ],
+                "hash": "tbd",
+                "architecture": "",
+                "description": description,
+                "license": "Freeware",
+                "notes": "If this application is useful to you, please consider donating to nirsoft.",
+                "checkver": {
+                    "url": "https://www.nirsoft.net/pad/" + name + ".xml",
+                    "xpath": "/XML_DIZ_INFO/Program_Info/Program_Version"
+                },
+                "autoupdate": {
+                    "url": download
+                }
+            }
+
+            if x64:
+                manifest.pop("url")
+                manifest.pop("hash")
+                manifest["autoupdate"] = {
+                    "architecture": {
+                        "64bit": {
+                            "url": download64
+                        },
+                        "32bit": {
+                            "url": download
+                        }
+                    },
+                }
+                manifest["architecture"] = {
+                    "64bit": {
+                    "url": download64,
+                    "hash": "tbd"
+                    },
+                    "32bit": {
+                        "url": download,
+                        "hash": "tbd"
+                    }
+                }
+            else:
+                manifest.pop("architecture")
+                manifest["url"] = download
+                manifest["hash"] = "tbd"
+
+            with open("bucket/" + name + ".json", "w") as j:
+                json.dump(manifest, j, indent=1)
+
+        except Exception as e:
+            print_exc()
