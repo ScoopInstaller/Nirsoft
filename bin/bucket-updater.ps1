05541d6d04d1e270b8b96a0f3c114e023d6db786
MCOfficer <mcofficer@gmx.de>
2020-02-28 09:50:22 +0100

    Remove bucket-updater.ps1

--- bin/bucket-updater.ps1
+++
@@ -1,8 +0,0 @@
-param(
-    # overwrite upstream param
-    [String]$upstream = "<user>/<repo>:master"
-)
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
-$autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
-$dir = "$psscriptroot/.." # checks the parent dir
-iex -command "$autopr -dir $dir -upstream $upstream $($args |% { "$_ " })"
